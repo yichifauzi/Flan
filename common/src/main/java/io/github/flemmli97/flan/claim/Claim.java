b3881f984be804ced951ac9d2359689cb415f4e6
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-06-23 23:44:33 +0200

    fix #303 admin claim with enter/leave text crashing

--- common/src/main/java/io/github/flemmli97/flan/claim/Claim.java
+++ common/src/main/java/io/github/flemmli97/flan/claim/Claim.java
@@ -638,9 +638,9 @@ public class Claim implements IPermissionContainer {
             return null;
         MutableComponent res;
         if (component.getContents() instanceof TranslatableContents trans) {
-            res = Component.translatable(trans.getKey(), this.world.getServer().getProfileCache().get(this.owner).map(GameProfile::getName).orElse("<UNKNOWN>"), this.claimName);
+            res = Component.translatable(trans.getKey(), this.isAdminClaim() ? "Admin" :this.world.getServer().getProfileCache().get(this.owner).map(GameProfile::getName).orElse("<UNKNOWN>"), this.claimName);
         } else if (component.getContents() instanceof LiteralContents comp) {
-            res = Component.translatable(comp.text(), this.world.getServer().getProfileCache().get(this.owner).map(GameProfile::getName).orElse("<UNKNOWN>"), this.claimName);
+            res = Component.translatable(comp.text(), this.isAdminClaim() ? "Admin" : this.world.getServer().getProfileCache().get(this.owner).map(GameProfile::getName).orElse("<UNKNOWN>"), this.claimName);
         } else {
             res = component.plainCopy();
         }
